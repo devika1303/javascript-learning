382b8bfe1a4d7cd522e98d6090ea801d81d1c67a
Gujjula Devika <devika1303@gmail.com>
2026-08-17 22:02:11 +0530

    condtional statements-grade caluclater

--- basic.js
+++ basic.js
@@ -11,35 +11,50 @@
 // console.log(college);
 // console.log(skills);
 
-// creating different vaiable names
-let firstName = "Devika";
-console.log(firstName);
+// // creating different vaiable names
+// let firstName = "Devika";
+// console.log(firstName);
 
-let city = "Delhi";
-console.log(city);
+// let city = "Delhi";
+// console.log(city);
 
-const country = "India";
-console.log(country);
+// const country = "India";
+// console.log(country);
 
-let age = 21;
-console.log(age);
+// let age = 21;
+// console.log(age);
 
-let isStudent = true;
-console.log(isStudent);
+// let isStudent = true;
+// console.log(isStudent);
 
-const isOnline = false;
-console.log(isOnline);
+// const isOnline = false;
+// console.log(isOnline);
 
-let score = 50;
-score = 80;
-console.log(score);
+// let score = 50;
+// score = 80;
+// console.log(score);
 
-let course = "HTML";
-course = "JavaScript";
-console.log(course);
+// let course = "HTML";
+// course = "JavaScript";
+// console.log(course);
 
-const year = 2026;
-console.log(year);
+// const year = 2026;
+// console.log(year);
 
-let marks = 85;
-console.log(marks);
+// let marks = 85;
+// console.log(marks);
+
+//grade caluclator
+let marks = 88;
+if(marks>=90){
+    console.log(" A Grade");
+}
+else if(marks>=75){
+    console.log("B Grade");
+}
+else if(marks >=50){
+    console.log("C Grade");
+}
+else{
+    console.log("Fail");
+}
