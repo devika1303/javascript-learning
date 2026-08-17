c47b047badcbd84771b78aad5363dd167061bff5
Gujjula Devika <devika1303@gmail.com>
2026-08-17 21:59:13 +0530

    creating different variables

--- basic.js
+++ basic.js
@@ -1,12 +1,45 @@
-//Creating variables in js
-let studentname = "devika";
-let age ="21";
-let course = "ECE";
-let college = "QISCET";
-let skills = "HTML, CSS ,JS";
+// // creating variable
+// let studentname = "devika";
+// let age ="21";
+// let course = "ECE";
+// let college = "QISCET";
+// let skills = "HTML, CSS ,JS";
 
-console.log(studentname);
+// console.log(studentname);
+// console.log(age);
+// console.log(course);
+// console.log(college);
+// console.log(skills);
+
+// creating different vaiable names
+let firstName = "Devika";
+console.log(firstName);
+
+let city = "Delhi";
+console.log(city);
+
+const country = "India";
+console.log(country);
+
+let age = 21;
 console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+const isOnline = false;
+console.log(isOnline);
+
+let score = 50;
+score = 80;
+console.log(score);
+
+let course = "HTML";
+course = "JavaScript";
 console.log(course);
-console.log(college);
-console.log(skills);
+
+const year = 2026;
+console.log(year);
+
+let marks = 85;
+console.log(marks);
