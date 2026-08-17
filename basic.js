e798b3464d1fcdd077c6b3811a52297af2844072
Gujjula Devika <devika1303@gmail.com>
2026-08-17 21:55:35 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,12 @@
+//Creating variables in js
+let studentname = "devika";
+let age ="21";
+let course = "ECE";
+let college = "QISCET";
+let skills = "HTML, CSS ,JS";
+
+console.log(studentname);
+console.log(age);
+console.log(course);
+console.log(college);
+console.log(skills);
